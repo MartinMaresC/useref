346f0abf98dba2e6cd88d4bc71efb0e8fd5d767d
Martin Mares <78748150+MartinMaresC@users.noreply.github.com>
2024-09-30 14:26:10 -0600

    App.js modified
    2 use ref created in order to store values and print them on screen, both inside DOM and as an alert. Alert was removed at the end as testing was successful.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
+import { useRef } from 'react';
 import './App.css';
 
 function App() {
+  const valorInput = useRef();  
+  const resultado = useRef();
+  const imprimirTexto = () => {
+    resultado.current.innerHTML = valorInput.current.value;
+  }
   return (
+
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input ref={valorInput}/>
+      <br></br>
+      <button onClick={imprimirTexto}>Imprimir texto</button>
+      <div ref={resultado}></div>
     </div>
   );
 }
